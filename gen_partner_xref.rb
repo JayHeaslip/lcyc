449ee94b0f0a47baee94dafa1a5cd8606e5a07d4
Jay Heaslip <jheaslip@comcast.net>
2018-04-04 07:03:25 -0400

    partner xref

--- gen_partner_xref.rb
+++ gen_partner_xref.rb
@@ -1,11 +1,15 @@
-all = Membership.all_active(:include => :people)
+# read resulting file in text editor (notepad), then cut and paste into an excell spreadsheet
+# under Data/Text to Columns, split at semicolon
+# sort by first column (partner last name)
+
+all = Membership.all_active.includes(:people)
 
 File.open('partner_xref.csv','w') do |o|
   all.each do |m|
-    member = m.people.where("MemberType = 'Member'").first
-    partner = m.people.where("MemberType = 'Partner'").first
+    member = m.people.where(MemberType: 'Member').first
+    partner = m.people.where(MemberType: 'Partner').first
     if partner && member.LastName != partner.LastName
-      o.puts "'#{partner.LastName}', '#{partner.FirstName}', 'see', '#{member.LastName}', '#{member.FirstName}'"
+      o.puts "#{partner.LastName}, #{partner.FirstName};see;#{member.LastName}, #{member.FirstName}"
     end
   end
 end
