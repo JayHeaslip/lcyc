9c01f078e10368b854b495b0abd47ee8ee8a1b81
Jay Heaslip <jheaslip@comcast.net>
2018-04-12 15:24:41 -0400

    fix hash syntax

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mailrobot'
 
-  default :from => 'No Reply <lcyc@members.lcyc.info>', :reply_to => 'lcyc@members.lcyc.info'
-  default :date => Proc.new { Time.now.localtime }
+  default from: 'No Reply <lcyc@members.lcyc.info>', reply_to: 'lcyc@members.lcyc.info'
+  default date: Proc.new { Time.now.localtime }
 
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ApplicationRecord
   belongs_to :mailing, optional: true
 
   has_attached_file :pdf
-  validates_attachment_content_type :pdf, :content_type => 'application/pdf'
-  validates_attachment_size :pdf, :less_than => 5.megabytes
+  validates_attachment_content_type :pdf, content_type: 'application/pdf'
+  validates_attachment_size :pdf, less_than: 5.megabytes
 
 end
