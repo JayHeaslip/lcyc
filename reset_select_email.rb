5968de41bc2455547e6f5f7f369a6ca864395cd8
Jay Heaslip <jheaslip@comcast.net>
2018-06-29 21:49:15 -0400

    fix reset email

--- reset_select_email.rb
+++ reset_select_email.rb
@@ -1,5 +1,5 @@
 # RAILS_ENV=production rails c
 # load 'reset_select_email.rb'
 
-p = People.where('select_email is true')
+p = Person.where('select_email is true')
 p.each {|e| e.select_email = false; e.save }
