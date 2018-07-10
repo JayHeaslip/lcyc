c07b288dc2f3fab258795f8f6719cf1d424b057e
Jay Heaslip <jheaslip@comcast.net>
2018-07-10 19:50:55 -0400

    limit mailing to 1 per day, decrease job interval to 30 seconds

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 Delayed::Worker.backend = :active_record
-Delayed::Worker.sleep_delay = 45
+Delayed::Worker.sleep_delay = 30
 Delayed::Worker.read_ahead = 2
 Delayed::Worker.max_attempts = 3
 
