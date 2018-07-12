f0e507be4a14377b55ef487e229cb5423b357624
Jay Heaslip <jheaslip@comcast.net>
2018-07-12 14:44:26 -0400

    log level

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -6,5 +6,5 @@ Delayed::Worker.read_ahead = 2
 Delayed::Worker.max_attempts = 3
 
 # note: this overrides config.log_level
-::ActiveRecord::Base.logger.level = Logger::INFO
+::ActiveRecord::Base.logger.level = Logger::WARN
 
