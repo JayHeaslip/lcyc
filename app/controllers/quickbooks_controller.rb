7559fce8a528e55b1619773aa48b03d312071ea1
Jay Heaslip <jheaslip@comcast.net>
2019-07-21 06:45:51 -0400

    member create & delete

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'openssl'
 require "base64"
 require 'json'
+require 'pp'
 
 class QuickbooksController < ApplicationController
 
@@ -27,20 +28,57 @@ class QuickbooksController < ApplicationController
         session[:realm_id] = params[:realmId]
       else
         "Something went wrong. Try the process again"
-      end
+        end
     else
       "Error: #{error}"
     end
   end
 
   def update_members
-    service = Quickbooks::Service::Customer.new
-    logger.info session[:access_token].class
-    
-    service.company_id = session[:realmID]
-    service.access_token = session[:access_token]
-    logger.info service
-    @customers = service.query()
+    if access_token = session[:access_token]
+      @api = QboApi.new(access_token: access_token, realm_id: session[:realm_id])
+      members = Membership.members.where('Status NOT IN ("Honorary", "Life")').includes(:people)
+      qb_members = @api.all(:customer)
+      @existing_qbo_members = {}
+      members.each do |m|
+        @existing_qbo_members[m.MailingName] = false
+      end
+
+      # for each membership, update the data in QBO
+      qb_members.each do |qbm|
+        display_name = qbm['DisplayName']
+        m = Membership.find_by_MailingName(display_name)
+        logger.info "looking up #{display_name}"
+        logger.info "m nil? #{m.nil?}"
+        logger.info m
+        update_member(qbm,m,display_name)
+      end
+
+      # add new members
+      count = 0
+      @existing_qbo_members.each do |k,v|
+        unless v # create the member in QBO
+          m = Membership.find_by_MailingName(k)
+          member_email = m.people.where('MemberType = "Member"').first.EmailAddress
+          member = {
+            "DisplayName": k,
+                    "BillAddr": { "Line1": m.StreetAddress,
+                                  "City":  m.City,
+                                  "CountrySubDivisionCode": m.State,
+                                  "PostalCode": m.Zip
+                                },
+                    "PrimaryEmailAddr": { "Address": member_email }
+          }
+          logger.info "Creating #{k}"
+          response = @api.create(:customer, payload: member)
+          count += 1
+          if (count % 20) == 0
+            sleep(40)
+          end
+        end
+      end
+      
+    end
     
   end
 
@@ -61,6 +99,15 @@ class QuickbooksController < ApplicationController
       token_endpoint: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
     )
     
+  end
+
+  def update_member(qbm, m, display_name)
+    if m # update a member
+      @existing_qbo_members[display_name] = true
+    else # delete from QBO, no longer in membership db
+      logger.info "deleting #{display_name}"
+      response = @api.deactivate(:customer, id: qbm['Id'])
+    end
   end
   
 end
