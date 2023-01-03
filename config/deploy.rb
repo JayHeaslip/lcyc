b7c023301d6acce0817bcb6315b75f67e2d83973
Jay Heaslip <jheaslip@comcast.net>
2023-01-03 07:00:15 -0500

    updated capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.17.1"
 
 set :application, "lcyc"
 set :repo_url, "https://github.com/JayHeaslip/lcyc.git"
