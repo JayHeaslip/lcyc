8724accdb44f64d214245db595d0cb94bf7f7d01
Jay Heaslip <jheaslip@comcast.net>
2023-01-04 09:11:26 -0500

    fix host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "members.lcyc.info" }
+  config.action_mailer.default_url_options = { host: "staging.lcyc.info" }
   
   config.action_mailer.smtp_settings = {
 	 :address     	       => "smtp.gmail.com",
