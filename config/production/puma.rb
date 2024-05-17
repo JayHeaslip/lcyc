647cf616d51293139ea62b8eafff86694be5fb6e
Jay Heaslip <jheaslip@comcast.net>
2024-05-17 09:16:03 -0400

    fix puma config

--- config/production/puma.rb
+++ config/production/puma.rb
@@ -9,11 +9,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT", 8001)
+port ENV.fetch("PORT", 8002)
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "staging" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
