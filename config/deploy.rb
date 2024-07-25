52a2fb6f9e0ef0618f2e331acf506e6943ed55c9
Jay Heaslip <jheaslip@comcast.net>
2024-07-25 07:05:26 -0400

    updated Capfile lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.19.1"
 
 set :application, "lcyc"
 set :repo_url, "https://github.com/JayHeaslip/lcyc.git"
