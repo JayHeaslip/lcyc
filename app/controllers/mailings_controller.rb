318e76805c16f254b7e7e53d72e38b5e06e5cca4
Jay Heaslip <jheaslip@comcast.net>
2024-08-22 13:44:43 -0400

    trix, standardrb updates

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -62,29 +62,29 @@ class MailingsController < ApplicationController
     @mailing = Mailing.find(params[:id])
 
     # check if it's been at least 8 hours since we sent a blast
-    last_email_sent_time = Mailing.order(sent_at: :desc).first.sent_at
-    last_email_sent_time = Time.now - 1.day if last_email_sent_time.nil?
+    # last_email_sent_time = Mailing.order(sent_at: :desc).first.sent_at
+    # last_email_sent_time = Time.now - 1.day if last_email_sent_time.nil?
 
     # if params[:test] || (Time.now > (last_email_sent_time + 8.hours))
-      @filter_emails = !params[:filter_emails].nil?
-      people = Person.email_list(@mailing.committee, @filter_emails)
-      if params[:test]
-        p = Person.find_by_EmailAddress(current_user.email)
-        if p.nil?
-          p = add_as_nonmember(current_user.email)
-        end
-        people = [p]
-        logger.info "Test email address: #{p.EmailAddress}"
-      else
-        @mailing.sent_at = Time.now
-        @mailing.save
+    @filter_emails = !params[:filter_emails].nil?
+    people = Person.email_list(@mailing.committee, @filter_emails)
+    if params[:test]
+      p = Person.find_by_EmailAddress(current_user.email)
+      if p.nil?
+        p = add_as_nonmember(current_user.email)
       end
+      people = [p]
+      logger.info "Test email address: #{p.EmailAddress}"
+    else
+      @mailing.sent_at = Time.now
+      @mailing.save
+    end
 
-      unless people.empty?
-        deliver_mail(people, @mailing, host, @filter_emails)
-        flash[:notice] = "Delivering mail."
-      end
-      redirect_to mailings_path
+    unless people.empty?
+      deliver_mail(people, @mailing, host, @filter_emails)
+      flash[:notice] = "Delivering mail."
+    end
+    redirect_to mailings_path
     # else
     #   formatted_time = (last_email_sent_time + 8.hours).strftime("%m/%d/%Y at %I:%M %p")
     #   flash[:error] = "You've sent a mailing within the last 8 hours, please wait until #{formatted_time} to send an email"
--- test/integration/mailings_test.rb
+++ test/integration/mailings_test.rb
@@ -77,15 +77,15 @@ class MailingsIntegrationTest < ActionDispatch::IntegrationTest
     assert_redirected_to mailings_url
   end
 
-###  test "send too soon" do
-###    t = Time.now
-###    old_email = mailings(:two)
-###    old_email.sent_at = t
-###    old_email.save
-###    post send_email_mailing_url(@mailing)
-###    formatted_time = (t + 8.hours).strftime("%m/%d/%Y at %I:%M %p")
-###    assert_equal "You've sent a mailing within the last 8 hours, please wait until #{formatted_time} to send an email", flash[:error]
-###  end
+  ###  test "send too soon" do
+  ###    t = Time.now
+  ###    old_email = mailings(:two)
+  ###    old_email.sent_at = t
+  ###    old_email.save
+  ###    post send_email_mailing_url(@mailing)
+  ###    formatted_time = (t + 8.hours).strftime("%m/%d/%Y at %I:%M %p")
+  ###    assert_equal "You've sent a mailing within the last 8 hours, please wait until #{formatted_time} to send an email", flash[:error]
+  ###  end
 
   test "send test mailing non-member email" do
     logout
