e1d8a9d40b6e142a7c87e772d7782508ab32dd16
Jay Heaslip <jheaslip@comcast.net>
2024-09-12 17:38:19 -0400

    mail test

--- test/mailers/mail_robot_test.rb
+++ test/mailers/mail_robot_test.rb
@@ -29,4 +29,22 @@ class MailRobotTest < ActionMailer::TestCase
     assert_equal [ @person.EmailAddress ], email.to
     assert_equal "[LCYC] #{@mailing.subject}", email.subject
   end
+
+  test "mailing with pdf" do
+    @person = people(:bob)
+    @mailing = mailings(:one)
+    puts "class is #{@mailing.class}"
+    @mailing.attachments["first.pdf"] = File.read("/home/vboxuser/rails/lcyc/test/fixtures/files/first.pdf")
+    @mailing.save
+    url_options = {}
+    email = MailRobot.mailing(url_options, @person, @mailing, "www.example.com")
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal [ "lcyc@members.lcyc.info" ], email.from
+    assert_equal [ @person.EmailAddress ], email.to
+    assert_equal "[LCYC] #{@mailing.subject}", email.subject
+  end
 end
