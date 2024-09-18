1ed30dffdd1eefe12fc85c67d874c22d399f1a1c
Jay Heaslip <jheaslip@comcast.net>
2024-09-18 14:02:06 -0400

    fix load rights

--- dump_rights.rb
+++ dump_rights.rb
@@ -1,5 +1,5 @@
 Role.all.each do |role|
-  f = File.open("#{role.name}.rights","w")
+  f = File.open("#{role.name}.rights", "w")
   role.rights.each do |right|
     f.puts "#{right.name}"
   end
--- load_rights.rb
+++ load_rights.rb
@@ -1,2 +1,9 @@
-rights = Dir.glob("*.rights")
-puts rights
+Dir.glob("*.rights").each do |file|
+  role_name = file.gsub(/.rights/, "")
+  role = Role.find_by(name: role_name)
+  role.rights = []
+  File.foreach(file) do |line|
+    role.rights << Right.find_by(name: line.chomp)
+  end
+  role.save
+end
