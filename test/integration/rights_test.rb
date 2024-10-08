3ac95d2947c125f3355f824eda6ac1ff28171684
Jay Heaslip <jheaslip@comcast.net>
2024-10-08 12:21:36 -0400

    upgrade to rails 7.2, ruby 3.3.5

--- test/integration/rights_test.rb
+++ test/integration/rights_test.rb
@@ -3,11 +3,14 @@ require "test_helper"
 class RightsIntegrationTest < ActionDispatch::IntegrationTest
   test "synchronize" do
     Right.synchronize_with_controllers
+    r = Right.find_by(name: "MembershipsController.new")
+    assert_equal "MembershipsController.new", r.name
   end
 
   test "synchronize removing right" do
     r = Right.new(name: "UsersController.bogus", controller: "users", action: "bogus")
     r.save
     Right.synchronize_with_controllers
+    assert_nil Right.find_by(name: "UsersController.bogus")
   end
 end
