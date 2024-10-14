791606bf269be65840aa47327aee0c5d21a17404
Jay Heaslip <jheaslip@comcast.net>
2024-10-14 07:05:33 -0400

    clean up rights sync

--- app/models/right.rb
+++ app/models/right.rb
@@ -10,39 +10,58 @@ class Right < ApplicationRecord
   # Right.synchronize_with_controllers
 
   def self.synchronize_with_controllers
-    # Load all the controller files (except application, admin, confirmations, unsubscribe, passwords, sessions, active_sessions)
-    controller_files = Dir[Rails.root.to_s + "/app/controllers/*_controller.rb"].reject do |e|
-      e =~ /\/application_controller.rb/ || e =~ /\/admin_controller.rb/ || e =~ /\/unsubscribe_controller.rb/ || e =~ /\/confirmations_controller.rb/ ||
-        e =~ /\/passwords_controller.rb/ || e =~ /\/sessions_controller.rb/ || e =~ /\/active_sessions_controller.rb/ || e =~ /\/roles_controller.rb/
-    end
-
-    # we need to load all the controllers...
-    controllers = []
-    controller_files.each do |file_name|
-      require file_name
-      controllers << extract_class_name(file_name)
-    end
+    load_controller_files
 
     # look at all rights, delete ones that don't have a matching controller
-    controllers_str_array = controllers.map { |e| e.to_s }
-    all.each do |r|
+    cleanup_rights_database
+
+    # add all controller actions to rights database
+    sync_with_database
+
+    # special case - MailingsController#deliver_mail - don't check authorization on this (run in delayed job)
+    find_by_name("MailingsController.deliver_mail").destroy
+  end
+
+  def self.extract_class_name(filename)
+    File.basename(filename).chomp(".rb").camelize.constantize
+  end
+
+  def self.load_controller_files
+    controller_files = Dir[Rails.root.to_s + "/app/controllers/*_controller.rb"]
+    remove_list = [ "\/application_controller.rb", "\/admin_controller.rb", "\/unsubscribe_controller.rb",
+                   "\/confirmations_controller.rb", "\/passwords_controller.rb", "\/sessions_controller.rb",
+                   "\/active_sessions_controller.rb", "\/roles_controller.rb/" ]
+    controller_files.delete_if do |c|
+      remove_list.index { |e| /#{e}/.match(c) }
+    end
+    @controllers = []
+    controller_files.each do |file_name|
+      require file_name
+      @controllers << extract_class_name(file_name)
+    end
+  end
+
+  def self.cleanup_rights_database
+    controllers_str_array = @controllers.map { |e| e.to_s }
+    Right.all.each do |r|
       controller = r.name.gsub(/\..*/, "")
       unless controllers_str_array.include?(controller)
         # puts "removing #{r.name}"
         r.destroy
       end
     end
+  end
 
+  def self.sync_with_database
     # Find the actions in each of the controllers, and add them to the database
-    controllers.each do |controller|
+    @controllers.each do |controller|
       controller.public_instance_methods(false).each do |action|
         next if /return_to_main|component_update|component|^_/.match?(action)
         if where("controller = ? AND action = ?", controller.controller_path, action).empty?
           new(name: "#{controller}.#{action}", controller: controller.controller_path, action: action).save!
         end
       end
-      # The following thanks to Tom Styles
-      # Then check to make sure that all the rights for that controller in the database
+      # Check to make sure that all the rights for that controller in the database
       # still exist in the controller itself
       where([ "controller = ?", controller.controller_path ]).each do |right_to_go|
         unless controller.public_instance_methods(false).include?(right_to_go.action.to_sym)
@@ -51,13 +70,5 @@ class Right < ApplicationRecord
         end
       end
     end
-
-    # special case - MailingsController#deliver_mail - don't check authorization on this (run in delayed job)
-    # puts "removing MailingsController.deliver_mail"
-    find_by_name("MailingsController.deliver_mail").destroy
-  end
-
-  def self.extract_class_name(filename)
-    File.basename(filename).chomp(".rb").camelize.constantize
   end
 end
