403a934253de8df7f59d8c7b8ccf5701c1c875e7
Jay Heaslip <jheaslip@comcast.net>
2026-04-16 06:21:25 -0400

    security update

--- test/system/user_flows_test.rb
+++ test/system/user_flows_test.rb
@@ -11,7 +11,7 @@ class UserTest < ApplicationSystemTestCase
     fill_in "password", with: "password"
     fill_in "password_confirmation", with: "password"
     click_on "Register"
-    assert_current_path login_path, wait: 3
+    assert_current_path login_path, wait: 5
     assert_text "Check your email for confirmation instructions."
   end
 end
