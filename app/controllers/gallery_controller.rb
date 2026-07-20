d9b0bd037b4e52a9976d652b10d3eb0f4eb7eed6
Jay Heaslip <jheaslip@comcast.net>
2026-07-20 16:25:22 -0400

    gallery tweaks

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,5 +1,5 @@
 class GalleryController < ApplicationController
   def index
-    @people = Person.joins(:profile_picture_attachment).with_attached_profile_picture
+    @people = Person.joins(:profile_picture_attachment).with_attached_profile_picture.order(:LastName, :FirstName)
   end
 end
